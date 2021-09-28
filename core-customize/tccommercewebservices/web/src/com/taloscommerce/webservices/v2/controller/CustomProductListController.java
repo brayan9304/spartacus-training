b3c64f00cd6b3752e61299c4b0023d5ea2e2064a
Felipe Rios <felipe.rios@talosdigital.com>
2021-09-28 09:17:51 -0500

    unused imports

--- core-customize/tccommercewebservices/web/src/com/taloscommerce/webservices/v2/controller/CustomProductListController.java
+++ core-customize/tccommercewebservices/web/src/com/taloscommerce/webservices/v2/controller/CustomProductListController.java
@@ -15,8 +15,6 @@ import de.hybris.platform.webservicescommons.swagger.ApiFieldsParam;
 import io.swagger.annotations.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
