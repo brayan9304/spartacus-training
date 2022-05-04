79b2e4f886f4e5fcbd16abd28243309abb69b5e6
Carlangasv <carloszzzxc@hotmail.com>
2022-05-04 11:34:19 -0500

    Implementación del endpoint para retornar un wishlist.

--- core-customize/tccommercewebservices/web/src/com/taloscommerce/webservices/v2/controller/SelectiveCartController.java
+++ core-customize/tccommercewebservices/web/src/com/taloscommerce/webservices/v2/controller/SelectiveCartController.java
@@ -1,9 +1,12 @@
 package com.taloscommerce.webservices.v2.controller;
 
 
+import com.taloscommerce.webservices.dto.wishlist.WishListWsDTO;
 import de.hybris.platform.commerceservices.order.CommerceCartModificationException;
+import de.hybris.platform.selectivecartfacades.data.Wishlist2Data;
 import de.hybris.platform.selectivecartfacades.impl.DefaultSelectiveCartFacade;
 import de.hybris.platform.webservicescommons.swagger.ApiBaseSiteIdParam;
+import de.hybris.platform.webservicescommons.swagger.ApiBaseSiteIdUserIdAndCartIdParam;
 import de.hybris.platform.webservicescommons.swagger.ApiFieldsParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -19,8 +22,8 @@ import java.util.List;
 import static com.taloscommerce.webservices.v2.controller.BaseController.DEFAULT_FIELD_SET;
 
 @Controller
-@RequestMapping(value = "/{baseSiteId}/selective-cart")
-public class SelectiveCartController {
+@RequestMapping(value = "/{baseSiteId}/users/{userId}/carts/{cartId}/selective-cart")
+public class SelectiveCartController extends BaseController{
 
     @Resource(name = "defaultSelectiveCartFacade")
     private DefaultSelectiveCartFacade defaultSelectiveCartFacade;
@@ -75,14 +78,16 @@ public class SelectiveCartController {
 
     }
 
-    @GetMapping(value = "/getWishList", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/getWishList")
     @ResponseStatus(value = HttpStatus.OK)
-    @ApiOperation(nickname = "getWishList", value = "get the wishlist of the current cart")
-    @ApiBaseSiteIdParam
-    public void getWishList(@ApiFieldsParam @RequestParam(defaultValue = DEFAULT_FIELD_SET) final String fields)
+    @ApiOperation(nickname = "getWishList", value = "get the wishlist for a cart of an specific user")
+    @ApiBaseSiteIdUserIdAndCartIdParam
+
+    public WishListWsDTO getWishList(@ApiFieldsParam @RequestParam(defaultValue = DEFAULT_FIELD_SET) final String fields)
             throws CommerceCartModificationException
     {
-
+        final Wishlist2Data wishlist2Data =  defaultSelectiveCartFacade.getWishlistForSelectiveCart();
+        return getDataMapper().map(wishlist2Data, WishListWsDTO.class, fields);
     }
 
 }
