966b2548d2770f1753086749a1d62c937c886b9b
Carlangasv <carloszzzxc@hotmail.com>
2022-05-12 11:42:59 -0500

    Where condition fixed in Dao

--- core-customize/tccore/src/com/taloscommerce/core/dao/wishlist/impl/DefaultCustomWishListDao.java
+++ core-customize/tccore/src/com/taloscommerce/core/dao/wishlist/impl/DefaultCustomWishListDao.java
@@ -8,19 +8,21 @@ import de.hybris.platform.wishlist2.model.Wishlist2EntryModel;
 
 import java.util.List;
 
+
 public class DefaultCustomWishListDao extends DefaultGenericDao<Wishlist2EntryModel> implements CustomWishListDao {
 
-    private static final String REF_QUERY_WISHLIST2_OLD_ENTRIES = "SELECT {"+Wishlist2EntryModel.PK+"} FROM {"+Wishlist2EntryModel._TYPECODE+"} ";
-//          + "WHERE DATEDIFF('dd', {"+Wishlist2EntryModel.ADDEDDATE+"},  CURRENT_DATE) > 0";
+	private static final String REF_QUERY_WISHLIST2_OLD_ENTRIES =
+			"SELECT {" + Wishlist2EntryModel.PK + "} FROM {" + Wishlist2EntryModel._TYPECODE + "} "
+					+ "WHERE DATEDIFF('dd', {" + Wishlist2EntryModel.ADDEDDATE + "},  CURRENT_DATE) > 1";
 
-    public DefaultCustomWishListDao() {
-        super(Wishlist2EntryModel._TYPECODE);
-    }
+	public DefaultCustomWishListDao() {
+		super(Wishlist2EntryModel._TYPECODE);
+	}
 
-    @Override
-    public List<Wishlist2EntryModel> getAllExpiredWishListEntries() {
-        FlexibleSearchQuery fsq = new FlexibleSearchQuery(REF_QUERY_WISHLIST2_OLD_ENTRIES);
-        SearchResult<Wishlist2EntryModel> result = this.getFlexibleSearchService().search(fsq);
-        return result.getResult();
-    }
+	@Override
+	public List<Wishlist2EntryModel> getAllExpiredWishListEntries() {
+		FlexibleSearchQuery fsq = new FlexibleSearchQuery(REF_QUERY_WISHLIST2_OLD_ENTRIES);
+		SearchResult<Wishlist2EntryModel> result = this.getFlexibleSearchService().search(fsq);
+		return result.getResult();
+	}
 }
